bec80c598e44e35e81c8e25c74e2ff6fd8ddda90
HanWeiguang <hanweiguang1@gmail.com>
2017-07-29 17:59:05 +0800

    修改entrypoint.sh
    如果用户不指定LOCAL_USER_ID，则以root运行

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Add local user
-# Either use the LOCAL_USER_ID if passed in at runtime or
-# fallback
+USER_ID=${LOCAL_USER_ID:-0}
 
-USER_ID=${LOCAL_USER_ID:-9001}
-
-echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
-export HOME=/home/user
-
-exec /usr/local/bin/gosu user "$@"
+if [ $USER_ID != "0" ]; then
+	echo "Starting with UID : $USER_ID"
+	useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+	export HOME=/home/user
+	exec /usr/local/bin/gosu user "$@"
+else
+	exec "$@"
+fi
