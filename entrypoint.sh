b6d3cec70312c3881634b5678ff3034dab6bc23f
HanWeiguang <hanweiguang1@gmail.com>
2017-07-30 21:34:30 +0800

    调整了一下格式

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 USER_ID=${LOCAL_USER_ID:-0}
 
-if [ $USER_ID != "0" ]; then
+if [ ${USER_ID} != "0" ]; then
 	echo "Starting with UID : $USER_ID"
-	useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+	useradd --shell /bin/bash -u ${USER_ID} -o -c "" -m user
 	export HOME=/home/user
 	exec /usr/local/bin/gosu user "$@"
 else
