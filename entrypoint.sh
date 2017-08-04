ec1e1f7d96550ea0b5bede7a3f0ff1127c7dfb36
HanWeiguang <hanweiguang1@gmail.com>
2017-08-04 16:38:20 +0800

    将工作目录切换到用户目录
    /root或者是/home/user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ if [ ${USER_ID} != "0" ]; then
 	echo "Starting with UID : $USER_ID"
 	useradd --shell /bin/bash -u ${USER_ID} -o -c "" -m user
 	export HOME=/home/user
+	cd /home/user
 	exec /usr/local/bin/gosu user "$@"
 else
 	exec "$@"
